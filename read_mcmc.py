3aa6e9dc0b08ec0a77a84a8d8267f9b2a27c746d
Patrick <patrickstaudt1@gmail.com>
2024-05-30 20:06:54 -0700

    Trying to get corner plots to show priors

--- read_mcmc.py
+++ read_mcmc.py
@@ -25,12 +25,80 @@ def read(samples_fname, consider_burnin=True, flat=True):
 
     return samples
 
-def corner_plot(samples_fname, consider_burnin=True):
+def corner_plot(samples_fname, consider_burnin=True, log_prior_function=None):
     import corner
+    import scipy
+    import copy
     import matplotlib.pyplot as plt
+    import numpy as np
+    from progressbar import ProgressBar
 
     flat_samples = read(samples_fname, consider_burnin=consider_burnin)
+    ndim = flat_samples.shape[1]
     fig = corner.corner(flat_samples, labels=['D', 'e', 'H', 'j', 'k'])
+    axs = np.array(fig.axes).reshape((ndim, ndim))
+
+    if log_prior_function is not None:
+        # For each plot on the diagonal, marginalize over all the other
+        # parameters.
+
+        # Initialize a placeholder for the ranges that the corner plot 
+        # currently shows for each of i
+        # the 
+        # parameters
+        xranges = np.zeros((ndim, 2))
+
+        # Initialize a placeholder for all parameter values that the corner 
+        # plot currently shows for each 
+        # of the 
+        # parameters
+        Xs = []
+
+        for i in range(ndim):
+            # For each parameter, get the values that the corner plot
+            # currently shows.
+            ax = axs[i, i]
+            patches = ax.patches
+            polygon = patches[0]
+            vertices = polygon.get_path().vertices
+            xs = vertices[:, 0] # All theta[i] values
+            Xs.append(xs)
+            
+            # Min and max theta[i] values
+            xranges[i] = np.array([xs.min(), xs.max()]) 
+
+        for i in range(1):
+            def func(n0, n1, n2, n3, ti):
+                '''
+                Convert the log_prior_function so the nuicance parameters are
+                the first four args and the parameter of interest is last so
+                scipy.integrate.nquad can integrate over the nuicance
+                parameters
+                '''
+                theta = np.array([n0, n1, n2, n3])
+                theta = np.insert(theta, i, ti)
+                return log_prior_function(theta)
+
+
+            ax = axs[i, i]
+            xs = Xs[i]
+            logys = np.zeros(len(xs))
+
+            # Our integration bounds correspond only to the nuicance parameters
+            # i.e. not the i'th parameter
+            nuicance_ranges = np.delete(xranges, i, axis=0)
+
+            # Perform the marginalization.
+            pbar = ProgressBar()
+            for j, x in enumerate(pbar(xs)):
+                logys[j] = scipy.integrate.nquad(
+                        func, nuicance_ranges, args=(x,)
+                )[0]
+            ys = np.exp(np.array(logys))
+
+            ax_twin = ax.twinx()
+            ax_twin.plot(xs, ys)
+
     plt.show()
 
     return None
